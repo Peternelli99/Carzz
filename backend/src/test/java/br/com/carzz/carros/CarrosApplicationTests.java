43e6484fb25e8963da8584f1006361703ea7e709
Mateus de Oliveira Peternelli <mateuspeternelli@gmail.com>
2023-07-24 23:43:10 -0300

    teste unitario

--- backend/src/test/java/br/com/carzz/carros/CarrosApplicationTests.java
+++ backend/src/test/java/br/com/carzz/carros/CarrosApplicationTests.java
@@ -1,13 +1,50 @@
 package br.com.carzz.carros;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import br.com.carzz.carros.controllers.ConcessionariaController;
+import br.com.carzz.carros.entities.Concessionaria;
+import br.com.carzz.carros.services.ConcessionariaService;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
+	@Autowired
+	private ConcessionariaService concessionariaService;
+
+	@Autowired
+	private ConcessionariaController concessionariaController;
+
 	@Test
-	void contextLoads() {
+	void TesteCriarConcessionaria() {
+		Concessionaria concessionariaTeste = new Concessionaria();
+		concessionariaTeste.setDataCriacao(new Date());
+		concessionariaTeste.setNome("loja de carros");
+		Concessionaria novoConcessionaria = concessionariaService.cadastrar(concessionariaTeste);
+		assertEquals(concessionariaTeste, novoConcessionaria);
+	}
+
+	@Test
+	void testeListarConcessionaria(){
+		List<Concessionaria> concessionariaBuscada = concessionariaService.listar();
+		assertEquals("loja de carros", concessionariaBuscada.get(concessionariaBuscada.size()-1).getNome());
+	}
+
+	@Test
+	void testeRemoverConcessionaria(){
+		ResponseEntity<HttpStatus> respostaAceita = new ResponseEntity<>(HttpStatus.ACCEPTED);
+		ResponseEntity<HttpStatus> resposta = concessionariaController.excluirConcessionaria(Long.valueOf(9));
+		assertEquals(respostaAceita, resposta);
+
 	}
 
 }
